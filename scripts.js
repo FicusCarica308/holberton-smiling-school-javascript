b86c9b7fd21e61a41d997f9161602cb6ead7fac1
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-12 07:26:34 +0000

    added beginning of task 2 carousel implementation

--- scripts.js
+++ scripts.js
@@ -8,8 +8,14 @@ window.onload = function main() {
         }
     }
 
-    function getQuotes() {
-        const url = "https://smileschool-api.hbtn.info/quotes"
+    function getItems(type) {
+        const url = `https://smileschool-api.hbtn.info/${type}`
+        let createElement = NaN;
+        if (type == "quotes"){
+            createElement = createQuote();
+        } else if (type == "popular-tutorials" || type == "latest-videos") {
+            createElement = createVideo();
+        }
         displayLoader(true);
         $.ajax({
             url: url,
@@ -20,7 +26,7 @@ window.onload = function main() {
             /*set timeout to show off loader element*/
             setTimeout(()=>{
                 for (item in data) {
-                    createQuote(data[item]);
+                    createElement(data[item]);
                 }
                 displayLoader(false);
             }, 0800);
@@ -30,23 +36,82 @@ window.onload = function main() {
         })
     }
 
-    function createQuote(dataItem) {
-        const ifFirst = `<div class="carousel-item active" id="quote-${dataItem.id}">`;
-        const ifNotFirst = `<div class="carousel-item" id="quote-${dataItem.id}">`;
-        $("#quote-container").append(`
-        ${(dataItem.id == 1) ? ifFirst : ifNotFirst}
-            <div class="d-flex justify-content-center flex-column flex-md-row align-items-center">
-                <img class="rounded-circle d-block p-5" src="${dataItem.pic_url}" alt="First slide" width="250" height="250">
-                <div class="d-flex flex-column p-2">
-                    <h5 class="text-light mb-5">${dataItem.text}</h5>
-                    <h4 class="text-light mb-0 font-weight-bold">${dataItem.name}</h4>
-                    <h5 class="text-light font-weight-light font-italic ">${dataItem.title}</h5>
+    /*DYNAMIC LOAD OF QUOTE CAROSEL*/
+
+    function createQuote() {
+        return function newQuote(dataItem) {
+            const ifFirst = `<div class="carousel-item active" id="quote-${dataItem.id}">`;
+            const ifNotFirst = `<div class="carousel-item" id="quote-${dataItem.id}">`;
+            $("#quote-container").append(`
+            ${(dataItem.id == 1) ? ifFirst : ifNotFirst}
+                <div class="d-flex justify-content-center flex-column flex-md-row align-items-center">
+                    <img class="rounded-circle d-block p-5" src="${dataItem.pic_url}" alt="First slide" width="250" height="250">
+                    <div class="d-flex flex-column p-2">
+                        <h5 class="text-light mb-5">${dataItem.text}</h5>
+                        <h4 class="text-light mb-0 font-weight-bold">${dataItem.name}</h4>
+                        <h5 class="text-light font-weight-light font-italic ">${dataItem.title}</h5>
+                    </div>
                 </div>
             </div>
-        </div>
-        `)
+            `);
+        };
     }
+    /*==================================================*/
 
+    /*DYNAMIC LOAD OF POPULAR TUTORIALS CAROSEL*/
 
-    getQuotes();
+    function createVideo() {
+        return function newVideo(dataItem) {
+            const ifFirst = `<div class="carousel-item active" id="video-${dataItem.id}">`;
+            const ifNotFirst = `<div class="carousel-item" id="video-${dataItem.id}">`;
+            $("#card-item-container").append(`
+            ${(dataItem.id == 1) ? ifFirst : ifNotFirst}
+                <div class="card w-75 mr-4 border-0 pl-sm-4 pl-md-1" style="width: 18rem;">
+                <div class="card-header card-header-custom" id="card-header-1">
+                    <img class="play-image" src="./images/play.png">
+                </div>
+                <div class="card-body p-2 p-md-3">
+                    <h5 class="card-title font-weight-bold pt-1">Diagonal Smile</h5>
+                    <p class="card-text text-secondary">Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod.</p>
+                    <div class="d-flex flex-row pt-3">
+                        <img class="rounded-circle" src="./images/profile_1.jpg" width="50">
+                        <p class="accent-colors pl-2 my-auto font-weight-bold">Phillip Massey</p>
+                    </div>
+                    <div class="d-flex flex-row text-center">
+                        <div class="mr-5 d-flex w-50 flex-row">
+                            <img src="./images/star_on.png" class="mr-1 mt-1" width="15" height="15">
+                            <img src="./images/star_on.png" class="mr-1 mt-1" width="15" height="15">
+                            <img src="./images/star_on.png" class="mr-1 mt-1" width="15" height="15">
+                            <img src="./images/star_on.png" class="mr-1 mt-1" width="15" height="15">
+                            <img src="./images/star_off.png" class="mr-1 mt-1" width="15" height="15">
+                        </div>
+                        <p class="mx-auto accent-colors font-weight-bold">8 min</p>
+                    </div>
+                </div>
+            </div>
+            `);
+        };
+    }
+    
+    /* carousel item override */
+
+    $('.carousel .carousel-item').each(function(){
+        var next = $(this).next();
+        if (!next.length) {
+        next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+        
+        for (var i=0;i<2;i++) {
+            next=next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+            
+            next.children(':first-child').clone().appendTo($(this));
+        }
+    });
+
+    getItems("quotes");
+    /*getItems("popular-tutorials");*/
 }
